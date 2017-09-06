7baa046c9b8edfa82676ccb0c107070e8a5e1d89
Che-Lun Juang <zacharyjuang@gmail.com>
2017-09-06 16:39:48 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sysbio_wiki_wordcloud',
-    version='1.1.1',
+    version='1.1.2',
     description="Making a word cloud from Dr. Christine Vogel's Systems Biology Wiki.",
     author="Che-Lun Juang",
     entry_points={
