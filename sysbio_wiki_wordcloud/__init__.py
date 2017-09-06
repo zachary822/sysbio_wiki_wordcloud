38822faa92b712f65ed8e1a7fbd34127345d4ffc
Che-Lun Juang <zacharyjuang@gmail.com>
2017-09-06 01:54:30 -0400

    Refactoring

--- sysbio_wiki_wordcloud/__init__.py
+++ sysbio_wiki_wordcloud/__init__.py
@@ -1,26 +1,39 @@
-from typing import Generator
+import os
+from typing import Generator, Set
 from urllib.parse import urldefrag
 
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.remote.webdriver import WebDriver
-from wordcloud import WordCloud
-import os
+from wordcloud import STOPWORDS, WordCloud
+from PIL import Image
 
-from wordcloud import STOPWORDS
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
 
-STOPWORDS |= set([x.strip() for x in open(
-    os.path.join(os.path.dirname(__file__), 'stopwords')).read().split('\n')])
+with open(os.path.join(os.path.dirname(__file__), 'stopwords')) as stopwords_file:
+    STOPWORDS |= set(x.strip() for x in stopwords_file.readlines())
 
 __all__ = ['save_word_cloud']
+
+
+def open_window(driver: WebDriver, url: str) -> None:
+    driver.execute_script('open("{}")'.format(url))
+
+
+def get_links(driver: WebDriver) -> Set[str]:
+    links = driver.find_elements_by_xpath("//div[@id='children68296313-0']//a[@href and not(@class)]")
+
+    return {urldefrag(l.get_attribute('href'))[0] for l in links}
 
 
 def get_text_from_page(driver: WebDriver) -> Generator[str, None, None]:
     texts = driver.find_elements_by_xpath(
         "//div[@id='main-content']/*[not(contains(@class, 'macro') or contains(@class, 'plugin'))]")
 
-    for t in texts:
-        yield t.text
+    yield from (t.text for t in texts)
 
 
 def get_text_from_pages(driver: WebDriver) -> Generator[str, None, None]:
@@ -29,26 +42,36 @@ def get_text_from_pages(driver: WebDriver) -> Generator[str, None, None]:
         yield from get_text_from_page(driver)
 
 
-def save_word_cloud(name, width: int = 1920, height: int = 1080) -> None:
-    driver = Chrome()
+def get_image(driver: WebDriver, width: int = 1920, height: int = 1080) -> Image:
+    for link in get_links(driver):
+        open_window(driver, link)
+
+    text = ' '.join(get_text_from_pages(driver))
+
+    wordcloud = WordCloud(width=width, height=height, stopwords=STOPWORDS).generate(text)
+
+    return wordcloud.to_image()
+
+
+def save_word_cloud(name, width: int = 1920, height: int = 1080, *args, **kwargs) -> None:
+    """
+    Get word cloud from Systems Biology Wiki
+
+    :param name: name of file
+    :param width: output file width
+    :param height: output file height
+    :param args: extra arguments passed to Pillow.Image.save
+    :param kwargs: extra arguments passed to Pillow.Image.save
+    :return: None
+    """
+    driver = Chrome(chrome_options=chrome_options)
 
     driver.get("https://wikis.nyu.edu/display/Vogel/Systems+Biology")
 
     try:
-        links = driver.find_elements_by_xpath("//div[@id='children68296313-0']//a[@href and not(@class)]")
+        image = get_image(driver, width, height)
 
-        for link in {urldefrag(l.get_attribute('href'))[0] for l in links}:
-            driver.execute_script('open("{}")'.format(link))
-
-        text = ' '.join(get_text_from_pages(driver))
-
-        wordcloud = WordCloud(width=width, height=height, stopwords=STOPWORDS).generate(text)
-
-        image = wordcloud.to_image()
-
-        image.save(name)
-
-    except NoSuchElementException:
-        pass
-
-    driver.quit()
+        image.save(name, *args, **kwargs)
+        
+    finally:
+        driver.quit()
